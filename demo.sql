c1f0a7bfb67187eb61f1a0b9074931c7633f298b
Ki3mON <maciej.kmak@op.pl>
2025-01-20 03:22:28 +0100

    fixed demo

--- demo.sql
+++ demo.sql
@@ -9,26 +9,43 @@ GO
 -------------------------------------------------------------------------------
 INSERT INTO dbo.Countries (CountryID, CountryName)
 VALUES
-  (1, 'Polska'),
-  (2, 'Niemcy');
+  (21, 'Zimbabwe'),
+  (22, 'Brasil');
+
+SELECT * FROM dbo.Countries
+    WHERE CountryID IN (21, 22);
+
+DELETE from dbo.Countries where CountryID = 21;
+DELETE from dbo.Countries where CountryID = 22;
 
 INSERT INTO dbo.Cities (CityID, CityName, CountryID)
 VALUES
-  (10, 'Kraków', 1),
-  (20, 'Warszawa', 1),
-  (30, 'Berlin', 2);
+  (101, 'Jasło', 1),
+  (102, 'Zgorzelec', 1),
+  (103, 'Berlin', 3);
 
+SELECT * FROM dbo.Cities
+    WHERE CityID IN (101, 102, 103);
+
+DELETE from dbo.Cities where CityID = 101;
+DELETE from dbo.Cities where CityID = 102;
+DELETE from dbo.Cities where CityID = 103;
 -------------------------------------------------------------------------------
 -- 3) Wstawiamy przykładowego studenta
 -------------------------------------------------------------------------------
 INSERT INTO dbo.Students (StudentID, FirstName, LastName, [Address], CityID, PostalCode, Phone, Email)
 VALUES
- (1000, 'Jan', 'Kowalski', 'ul. Długa 10', 10, '30-000', '500123456', 'jan.kowalski@example.com');
+ (1501, 'Jan', 'Kowalski', 'ul. Długa 10', 10, '30-000', '500123456', 'jan.kowalski@example.com');
 
 INSERT INTO dbo.Students (StudentID, FirstName, LastName, [Address], CityID, PostalCode, Phone, Email)
 VALUES
- (1001, 'Maria', 'Nowak', 'ul. Krótka 3', 20, '00-123', '501234567', 'maria.nowak@example.com');
+ (1502, 'Maria', 'Nowak', 'ul. Krótka 3', 20, '00-123', '501234567', 'maria.nowak@example.com');
 
+SELECT * FROM dbo.Students
+    WHERE StudentID IN (1501, 1502);
+
+DELETE from dbo.Students where StudentID = 1501;
+DELETE from dbo.Students where StudentID = 1502;
 -------------------------------------------------------------------------------
 -- 4) Wstawiamy przykładowego nauczyciela i tłumacza
 -------------------------------------------------------------------------------
@@ -40,26 +57,46 @@ INSERT INTO dbo.Translators (TranslatorID, FirstName, LastName, HireDate, Phone,
 VALUES
  (300, 'Tomasz', 'Tlumacz', '2021-03-10', '444-999-333', 'tomasz.tlumacz@example.com');
 
+SELECT * FROM dbo.Teachers
+    WHERE TeacherID = 200;
+SELECT * FROM dbo.Translators
+    WHERE TranslatorID = 300;
+
 -------------------------------------------------------------------------------
 -- 5) Dodajemy przykładowe języki i przypisujemy do nauczyciela/tłumacza
 -------------------------------------------------------------------------------
 INSERT INTO dbo.Languages (LanguageID, LanguageName)
 VALUES
- (1, 'Polski'),
- (2, 'Angielski'),
- (3, 'Niemiecki');
+ (16, 'Sith'),
+ (17, 'Klingon');
 
--- Nauczyciel umie język polski i angielski
 INSERT INTO dbo.TeacherLanguages (TeacherID, LanguageID)
 VALUES
  (200, 1),
  (200, 2);
 
--- Tłumacz obsługuje angielski i niemiecki
 INSERT INTO dbo.TranslatorsLanguages (TranslatorID, LanguageID)
 VALUES
  (300, 2),
  (300, 3);
+
+SELECT * FROM dbo.Languages
+    WHERE LanguageID IN (16, 17);
+
+SELECT * FROM dbo.TeacherLanguages
+    WHERE TeacherID = 200
+    ORDER BY LanguageID;
+
+SELECT * FROM dbo.TranslatorsLanguages
+    WHERE TranslatorID = 300
+    ORDER BY LanguageID;
+
+DELETE from dbo.Languages where LanguageID = 16;
+DELETE from dbo.Languages where LanguageID = 17;
+
+DELETE from dbo.TeacherLanguages where TeacherID = 200;
+DELETE from dbo.TranslatorsLanguages where TranslatorID = 300;
+
 
 -------------------------------------------------------------------------------
 -- 6) Wstawiamy coś do tabeli Activities, aby zaprezentować np. tworzenie kursu
@@ -69,6 +106,11 @@ VALUES
 INSERT INTO dbo.Activities (ActivityID, Price, Title, Active)
 VALUES
  (900, 99.00, 'TestActivity Manual', 1);
+
+SELECT * FROM dbo.Activities
+    WHERE ActivityID = 900;
+
+DELETE from dbo.Activities where ActivityID = 900;
 
 -------------------------------------------------------------------------------
 -- 7) Prezentacja wywołania PROCEDUR
@@ -85,9 +127,18 @@ EXEC dbo.spAddCourse
     @ActivityActive     = 1;
 GO
 
+SELECT * FROM dbo.Courses
+    WHERE CourseName = 'Kurs Programowania w C#';
+
+DELETE from dbo.Courses where CourseName = 'Kurs Programowania w C#';
 -- Załóżmy, że procedura zwróci nam CreatedCourseID = np. 100
 
 -- 7.2 Rejestracja studenta na kurs (CourseID=100, StudentID=1000)
+
+--TUTAJ UPEWNIJ SIE ZE TAKI KURS ISTNIEJE
+SELECT * FROM dbo.Courses
+    WHERE CourseID = 100;
+
 EXEC dbo.spRegisterStudentInCourse
      @CourseID  = 100,
      @StudentID = 1000;
@@ -106,7 +157,7 @@ GO
 
 -- 7.4 Aktualizacja ceny aktywności (np. bo zmieniamy ofertę)
 EXEC dbo.spUpdateActivityPrice
-     @ActivityID = 900,
+     @ActivityID = 2137,
      @NewPrice   = 149.99;
 GO
 
@@ -115,8 +166,8 @@ EXEC dbo.spAddWebinar
      @WebinarName        = 'Nowy Webinar',
      @WebinarDescription = 'Opis webinaru o bazach danych',
      @WebinarPrice       = 300,
-     @TeacherID          = 200,       -- Adam Nauczyciel
-     @LanguageID         = 2,         -- angielski
+     @TeacherID          = 200,
+     @LanguageID         = 2,
      @VideoLink          = 'https://example.com/webinarLive',
      @WebinarDate        = '2025-01-15 18:00',
      @DurationTime       = '02:00',
@@ -125,6 +176,10 @@ EXEC dbo.spAddWebinar
      @ActivityActive     = 1;
 GO
 
+DELETE from dbo.Webinars where WebinarName = 'Nowy Webinar';
+
+SELECT * FROM dbo.Webinars
+    WHERE WebinarName = 'Nowy Webinar';
 -------------------------------------------------------------------------------
 -- 8) Przykład użycia widoku
 -------------------------------------------------------------------------------
@@ -161,12 +216,6 @@ SELECT dbo.ufnCountActiveCoursesInPeriod('2024-01-01','2025-01-01') AS ActiveCou
 GO
 
 -------------------------------------------------------------------------------
--- 10) Przykład generowania raportu
--------------------------------------------------------------------------------
--- spGenerateFinancialReport - zlicza przychody z Activities
-EXEC dbo.spGenerateFinancialReport;
-GO
-
 -- spGetDebtors - lista dłużników (brak płatności)
 EXEC dbo.spGetDebtors;
 GO
@@ -176,10 +225,13 @@ GO
 -------------------------------------------------------------------------------
 -- a) Usunięcie kursu powinno wywołać TR_Courses_AfterDelete (o ile jest w DB).
 --    Najpierw sprawdzimy co mamy:
+INSERT INTO dbo.Courses (CourseID,ActivityID, CourseName, CourseDescription, CoursePrice, CourseCoordinatorID)
+VALUES
+ (100, 2137,'Kurs Testowy', 'Opis kursu testowego', 1000, 2);
 SELECT * FROM dbo.Courses WHERE CourseID = 100;
 
 --  O ile chcemy zademonstrować usunięcie:
--- DELETE FROM dbo.Courses WHERE CourseID = 100;
+DELETE FROM dbo.Courses WHERE CourseID = 100;
 
 -------------------------------------------------------------------------------
 -- KONIEC Skryptu Demonstracyjnego
